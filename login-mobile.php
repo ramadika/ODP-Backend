2dadafef9cd2d00beba37afe364dc9647018fa37
ramadika <rzdka18@gmail.com>
2021-03-03 08:10:53 +0700

    login access

--- login-mobile.php
+++ login-mobile.php
@@ -21,24 +21,38 @@ if(isset($User)
 	&& !empty(trim($User)) 
 	&& !empty(trim($Pass))
 	){
+        $Pass = md5($Pass);
         
         $userLog = mysqli_query($db_conn,"SELECT * FROM `akun` WHERE `Username`='$User' and `Password`='$Pass'");
         $row = mysqli_fetch_array($userLog,MYSQLI_ASSOC);
 
         if(mysqli_num_rows($userLog) > 0){
 
-            if ($row['Kode_Pegawai'] == "F0001") {
+            // if ($row['Kode_Pegawai'] == "F0001") {
+            //     $idUser = $row['User_ID'];
+            //     $userToken = md5($idUser);
+            //     echo json_encode(["success"=>1,"msg"=>"Login Successfully","User_ID"=>$userToken]);
+            // }
+            // else {
+            //     echo json_encode(["success"=>0,"msg"=>"Don't have Access"]);
+            // }
+
+            $verifikasi = $row['verifikasi'];
+            if ($verifikasi == "1" || $verifikasi == '1' || $verifikasi == 1) {
                 $idUser = $row['User_ID'];
                 $userToken = md5($idUser);
-                echo json_encode(["success"=>1,"msg"=>"Login Successfully","User_ID"=>$userToken]);
+                echo json_encode(["success"=>1,"msg"=>"Login Successfully","User_ID"=>$userToken,"verifikasi"=>$verifikasi]);
+            } 
+            else if ($verifikasi == "2" || $verifikasi == '2' || $verifikasi == 2) {
+                echo json_encode(["success"=>0,"msg"=>"Don't have Access","verifikasi"=>$verifikasi]);
             }
             else {
-                echo json_encode(["success"=>0,"msg"=>"Don't have Access"]);
+                echo json_encode(["success"=>0,"msg"=>"Don't have Access","verifikasi"=>$verifikasi]);
             }
 
         }
         else{
-            echo json_encode(["success"=>0,"msg"=>"Incorrect username or password"]);
+            echo json_encode(["success"=>0,"msg"=>"Incorrect username or password","verifikasi"=>"3"]);
         }
 }
 else{
