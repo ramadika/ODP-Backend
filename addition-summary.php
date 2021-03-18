bbbdbbf82d44c1700f5cbe3ae7209efb334555f4
ramadika <rzdka18@gmail.com>
2021-03-18 09:51:00 +0700

    add

--- addition-summary.php
+++ addition-summary.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 require 'db_connection.php';
 
-$AdditionSummary = mysqli_query($db_conn,"SELECT Tanggal_Instalasi, COUNT(*) AS Installation FROM `port` GROUP BY Tanggal_Instalasi");
+$AdditionSummary = mysqli_query($db_conn,"SELECT date_format(Tanggal_Instalasi, '%M %e, %Y') as formatted_date, COUNT(*) AS Installation FROM `port` GROUP BY Tanggal_Instalasi");
 if(mysqli_num_rows($AdditionSummary) > 0){
     $count_Addition = mysqli_fetch_all($AdditionSummary,MYSQLI_ASSOC);
     echo json_encode(["success"=>1,"additionSummary"=>$count_Addition]);
