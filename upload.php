2f3d69c047cb0cfd0827420e7ace900dbcdf879a
ramadika <rzdka18@gmail.com>
2021-04-07 15:36:22 +0700

    upload

--- upload.php
+++ upload.php
@@ -1,20 +1,31 @@
 <?php
 
-$attachment = basename($_FILES["image"]["name"]);
-$target_dir = "image/";
-$target_file = $target_dir . $attachment;
-move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+    $attachment = basename($_FILES["image"]["name"]);
+    $target_dir = "image/";
+    $target_file = $target_dir . $attachment;
+    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
 
-$desc  = $_POST['desc'];
+    $desc  = $_POST['desc'];
 
-if(isset($desc)   
-	&& !empty(trim($desc)) 
-	){
-        
-    echo json_encode(["success"=>1,"message"=>"Data Updated.","desc"=>$desc]);
-}
-else{
-    echo json_encode(["success"=>0,"message"=>"Please fill all the required fields!"]);
-}
+    if(isset($desc)   
+        && isset($attachment) 
+        && !empty(trim($desc)) 
+        && !empty(trim($attachment))
+        ){
+
+        $upload = mysqli_query($db_conn,"UPDATE `odp` 
+                                                SET `attachment`='$attachment'
+                                                WHERE `ODP_ID`='$ODPID'");
+        if($upload){
+            echo json_encode(["success"=>1,"message"=>"Data Uploaded.","desc"=>$desc]);
+        }
+        else{
+            echo json_encode(["success"=>0,"message"=>"Data Not Uploaded!"]);
+        }
+            
+    }
+    else{
+        echo json_encode(["success"=>0,"message"=>"Please fill all the required fields!"]);
+    }
 
 ?>
